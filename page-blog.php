65a4756e262a63e3b4ae5963d0fb1035b0b76d02
MICHAEL\makel <wing.michaelmia@gmail.com>
2025-10-28 02:32:02 +0800

    pagination fix

--- page-blog.php
+++ page-blog.php
@@ -147,7 +147,7 @@ get_header();
                 echo '</div>';
 
                 // Pagination
-                echo '<div class="mt-5">';
+                echo '<div class="mt-5 post-pagination">';
                 echo paginate_links( array(
                     'total'   => $query->max_num_pages,
                     'current' => $paged,
